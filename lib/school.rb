ab202325451d8a99db58580f828e79a592001633
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-27 23:48:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    {}
+  end
+
+  def add_student(student_name, student_grade)
+    roster[student_grade]
+    roster[student_grade] = []
+    roster[student_grade] << student_name
+  end
+
+end
